3c8e7d0b8933211d44829f47b10266bb05bb2342
Hamsa <hamsa@mapletechspace.com>
2026-01-20 15:06:19 +0530

    bu gixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
+import { nitro } from 'nitro/vite'
 import tailwindcss from '@tailwindcss/vite'
 const config = defineConfig({
   plugins: [
@@ -10,6 +11,7 @@ const config = defineConfig({
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
+    nitro(),
     tailwindcss(),
     viteReact({
       babel: {
